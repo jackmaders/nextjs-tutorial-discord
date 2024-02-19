e79131b7728dfacf2c8ac9ddc7fed7b918820115
Jack Maders <jackwmaders@gmail.com>
2024-02-19 14:59:08 +0000

    add create server API

---
+++ app/api/servers/route.ts
@@ -0,0 +1,35 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MemberRole } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { name, imageUrl } = await req.json();
+    const profile = await currentProfile();
+
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+
+    const { id: profileId } = profile;
+
+    const server = await db.server.create({
+      data: {
+        profileId,
+        name,
+        imageUrl,
+        inviteCode: crypto.randomUUID(),
+        channels: {
+          create: [{ name: "general", profileId }],
+        },
+        members: {
+          create: [{ profileId, role: MemberRole.ADMIN }],
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.error("[SERVERS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
